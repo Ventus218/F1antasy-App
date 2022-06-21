8de13f1f0529422e99825bfa4a44cea4e21ab64a
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2022-06-21 13:02:42 +0200

    Add O5 - Visualizzazione di tutte le Motorizzazioni del Campionato corrente in ordine decrescente di prezzo

---
+++ Query/O5 - Visualizzazione di tutte le Motorizzazioni del Campionato corrente in ordine decrescente di prezzo.sql
@@ -0,0 +1,13 @@
+SELECT pm.NomeMotorizzazione, pm.Prezzo
+FROM prezzo_motorizzazione pm
+WHERE pm.AnnoCampionato = (SELECT gpp.AnnoCampionato
+                         FROM gran_premio_programmato gpp
+                         ORDER BY Data DESC
+                         LIMIT 1)
+AND pm.DataGranPremio = (SELECT gpp.Data
+                         FROM gran_premio_programmato gpp
+                         ORDER BY Data DESC
+                         LIMIT 1)
+ORDER BY pm.Prezzo DESC;
+
+/* DA TESTARE */
