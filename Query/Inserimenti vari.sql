31bd067c341ffac97da9cba0bd6590e81b6d5b62
Dario Berti <dario.berti@studio.unibo.it>
2022-06-21 12:17:16 +0200

    O2 moved to his own file

--- Query/Inserimenti vari.sql
+++ Query/Inserimenti vari.sql
@@ -1,29 +1,3 @@
-INSERT INTO PILOTA(Codice, Nome, Cognome)
-    VALUES
-           (1, 'Lewis', 'Hamilton'),
-           (2, 'Bottas', 'Valtteri'),
-           (3, 'Max', 'Verstappen'),
-           (4, 'Sergio', 'Perez'),
-           (5, 'Daniel', 'Ricciardo'),
-           (6, 'Lando', 'Norris'),
-           (7, 'Sebastian', 'Vettel'),
-           (8, 'Lance', 'Stroll'),
-           (9, 'Esteban', 'Ocon'),
-           (10, 'Fernando', 'Alonso'),
-           (11, 'Charles', 'Leclerc'),
-           (12, 'Carlos', 'Sainz'),
-           (13, 'Pierre', 'Gasly'),
-           (14, 'Yuki', 'Tsunoda'),
-           (15, 'Kimi', 'Raikkonen'),
-           (16, 'Antonio', 'Giovinazzi'),
-           (17, 'Mick', 'Schumacher'),
-           (18, 'Nikita', 'Mazepin'),
-           (19, 'George', 'Russell'),
-           (20, 'Nicholas', 'Latifi'),
-           (21, 'Alexander', 'Albon'),
-           (22, 'Guanyu', 'Zhou'),
-           (23, 'Kevin', 'Magnussen');
-
 INSERT INTO MOTORIZZAZIONE(Nome)
     VALUES
         ('Ferrari'),
@@ -54,6 +28,32 @@ INSERT INTO GRAN_PREMIO(Stato, Nome, NumeroGiri, LunghezzaCircuito)
         ('Azerbaijan', 'Baku', 68, 3100),
         ('Spagna', 'Catalogna', 69, 3000),
         ('Principato di Monaco', 'Monte Carlo', 70, 3000);
+
+INSERT INTO PILOTA(Codice, Nome, Cognome)
+VALUES
+    (1, 'Lewis', 'Hamilton'),
+    (2, 'Bottas', 'Valtteri'),
+    (3, 'Max', 'Verstappen'),
+    (4, 'Sergio', 'Perez'),
+    (5, 'Daniel', 'Ricciardo'),
+    (6, 'Lando', 'Norris'),
+    (7, 'Sebastian', 'Vettel'),
+    (8, 'Lance', 'Stroll'),
+    (9, 'Esteban', 'Ocon'),
+    (10, 'Fernando', 'Alonso'),
+    (11, 'Charles', 'Leclerc'),
+    (12, 'Carlos', 'Sainz'),
+    (13, 'Pierre', 'Gasly'),
+    (14, 'Yuki', 'Tsunoda'),
+    (15, 'Kimi', 'Raikkonen'),
+    (16, 'Antonio', 'Giovinazzi'),
+    (17, 'Mick', 'Schumacher'),
+    (18, 'Nikita', 'Mazepin'),
+    (19, 'George', 'Russell'),
+    (20, 'Nicholas', 'Latifi'),
+    (21, 'Alexander', 'Albon'),
+    (22, 'Guanyu', 'Zhou'),
+    (23, 'Kevin', 'Magnussen');
 
 INSERT INTO POSIZIONE_PUNTEGGIO(Posizione, Punteggio)
     VALUES
@@ -238,39 +238,6 @@ VALUES
     (2022, '2022-05-22', 'Renault',        8000000),
     (2022, '2022-05-22', 'Red Bull',        7000000);
 
-# O2 - Inizializzazione della Squadra (scelta Piloti e Motorizzazione) ----- NON FUNZIONA!!!!!
-START TRANSACTION;
-INSERT INTO SQUADRA(AnnoCampionato, DataGranPremio, UsernameUtente, ScambiEffettuati, BudgetRimanente, NomeMotorizzazione)
-VALUE
-    (2021,
-     '2021-05-22',
-     'CiccioCarluz',
-     0,
-     100000000 - (SELECT Prezzo FROM PREZZO_MOTORIZZAZIONE
-                            WHERE AnnoCampionato = 2021
-                            AND DataGranPremio = '2021-05-22'
-                            AND NomeMotorizzazione = 'Ferrari'),
-    'Ferrari');
-
-INSERT INTO SCELTA_PILOTA(CodicePilota, AnnoCampionato, DataGranPremio, UsernameUtente)
-VALUES
-    (1, 2021, '2021-05-22', 'CiccioCarluz'),
-    (2, 2021, '2021-05-22', 'CiccioCarluz'),
-    (3, 2021, '2021-05-22', 'CiccioCarluz'),
-    (4, 2021, '2021-05-22', 'CiccioCarluz');
-
-UPDATE SQUADRA
-set  BudgetRimanente = BudgetRimanente - (select sum(Prezzo) as PrezzoTotale from PREZZO_PILOTA
-                                                                    where AnnoCampionato = 2021
-                                                                    and DataGranPremio = '2021-05-22'
-                                                                    and (CodicePilota = 1 or CodicePilota = 2 or CodicePilota = 3 or CodicePilota = 4))
-where AnnoCampionato = 2021
-and DataGranPremio = '2021-05-22'
-and UsernameUtente = 'CiccioCarluz';
-COMMIT;
-
-ALTER TABLE SQUADRA
-ADD CONSTRAINT CHK_BudgetRimanente_Positive CHECK (SQUADRA.BudgetRimanente >= 0);
 
 
 delete from SQUADRA where true;
