bc31d7ef4cc872e6d65a360d10c063cb590dad41
Alessandro Venturini <ales.ventus21@gmail.com>
2022-06-28 12:46:29 +0200

    Create O13 - Creazione Classifica Privata.sql

---
+++ Query/O13 - Creazione Classifica Privata.sql
@@ -0,0 +1,7 @@
+# O13 - Creazione Classifica Privata
+
+INSERT INTO CLASSIFICA_PRIVATA(Nome, NumeroPartecipanti)
+VALUE ('Winners', 0)
+
+
+# Subito dopo eseguire almeno una O14 per rispettare il vincolo di avere sempre almeno un UTENTE in ogni CLASSIFICA_PRIVATA
