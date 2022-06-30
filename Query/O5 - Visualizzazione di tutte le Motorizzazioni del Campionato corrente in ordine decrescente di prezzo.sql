202b02f1ff13db4e340198ca68533db1521dc8fe
Alessandro Venturini <ales.ventus21@gmail.com>
2022-06-30 10:16:45 +0200

    Fixed O5

--- Query/O5 - Visualizzazione di tutte le Motorizzazioni del Campionato corrente in ordine decrescente di prezzo.sql
+++ Query/O5 - Visualizzazione di tutte le Motorizzazioni del Campionato corrente in ordine decrescente di prezzo.sql
@@ -1,15 +1,13 @@
 # O5 - Visualizzazione di tutte le Motorizzazioni del Campionato corrente in ordine decrescente di prezzo
 
+WITH ULTMO_GRAN_PREMIO_PROGRAMMATO_CONCLUSO (AnnoCampionato, Data) AS (
+    SELECT AnnoCampionato, Data
+    FROM GRAN_PREMIO_PROGRAMMATO
+    WHERE Concluso = TRUE
+    ORDER BY AnnoCampionato DESC, Data DESC
+    LIMIT 1)
 SELECT pm.NomeMotorizzazione, pm.Prezzo
-FROM prezzo_motorizzazione pm
-WHERE pm.AnnoCampionato = (SELECT gpp.AnnoCampionato
-                         FROM gran_premio_programmato gpp
-                         ORDER BY Data DESC
-                         LIMIT 1)
-AND pm.DataGranPremio = (SELECT gpp.Data
-                         FROM gran_premio_programmato gpp
-                         ORDER BY Data DESC
-                         LIMIT 1)
+FROM prezzo_motorizzazione pm JOIN ULTMO_GRAN_PREMIO_PROGRAMMATO_CONCLUSO U
+WHERE pm.AnnoCampionato = U.AnnoCampionato
+AND pm.DataGranPremio = U.Data
 ORDER BY pm.Prezzo DESC;
-
-/* DA TESTARE */
