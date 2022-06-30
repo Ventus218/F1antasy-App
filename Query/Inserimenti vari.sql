2468bf301004f179314ab4d742e12ef53f754e25
Alessandro Venturini <ales.ventus21@gmail.com>
2022-06-30 10:15:19 +0200

    Remove useless query

--- Query/Inserimenti vari.sql
+++ Query/Inserimenti vari.sql
@@ -235,13 +235,3 @@ VALUES
     (2022, '2022-05-22', 'Ferrari',         9000000),
     (2022, '2022-05-22', 'Renault',        8000000),
     (2022, '2022-05-22', 'Red Bull',        7000000);
-
-# O4 - Visualizzazione della Squadra per un certo Gran Premio
-SELECT *
-FROM UTENTE U JOIN GRAN_PREMIO_PROGRAMMATO GPP JOIN SCELTA_PILOTA SP
-ON (SP.UsernameUtente = U.Username AND SP.AnnoCampionato = GPP.AnnoCampionato AND SP.DataGranPremio = GPP.DATA)
-WHERE U.Username = 'CiccioCarluz'
-AND GPP.AnnoCampionato = 2021
-AND GPP.Data = '2021-05-22';
-
-
