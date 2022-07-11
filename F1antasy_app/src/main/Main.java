15993ae1fccbf7eb44842d2512336ed2c06553b7
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2022-07-11 10:55:58 +0200

    Add app icon

--- F1antasy_app/src/main/Main.java
+++ F1antasy_app/src/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,7 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/main.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("F1antasy");
+        primaryStage.getIcons().add(new Image("main/asset/app_icon.png"));
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
