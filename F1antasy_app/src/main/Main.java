8574d6196db8d92bcb1ad7ee1f053944fedb8408
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2022-07-11 11:15:51 +0200

    Update main

--- F1antasy_app/src/main/Main.java
+++ F1antasy_app/src/main/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/main.fxml"));
         primaryStage.setTitle("F1antasy");
-        primaryStage.getIcons().add(new Image("main/asset/app_icon.png"));
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("asset/app_icon.png")));
         primaryStage.setScene(new Scene(root, 1080, 720));
         primaryStage.setMinWidth(1080);
         primaryStage.setMinHeight(720);
