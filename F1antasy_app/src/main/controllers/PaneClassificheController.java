145b8148cb3e7bd311b51bff960b0d7f5d37b04f
Alessandro Venturini <ales.ventus21@gmail.com>
2022-07-11 10:32:41 +0200

    Moved controllers and views in their own packages.

---
+++ F1antasy_app/src/main/controllers/PaneClassificheController.java
@@ -0,0 +1,14 @@
+package main.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PaneClassificheController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ F1antasy_app/src/main/controllers/TabPaneController.java
@@ -0,0 +1,22 @@
+package main.controllers;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TabPaneController implements Initializable {
+
+    @FXML
+    private PaneSquadraController paneSquadraController;
+
+    @FXML
+    private PaneClassificheController paneClassificheController;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
