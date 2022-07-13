554bc5905074771c1c9b04ca6cb1ac2bad2c74a7
Alessandro Venturini <ales.ventus21@gmail.com>
2022-07-13 18:50:46 +0200

    FIxed. Now always returns a result as expected.

--- Query/O4 - Visualizzazione Piloti Campionato Corrente.sql
+++ Query/O4 - Visualizzazione Piloti Campionato Corrente.sql
@@ -1,16 +1,17 @@
 # O4 - Visualizzazione di tutti i Piloti del Campionato corrente in ordine decrescente di prezzo
+
+WITH ULTMO_GRAN_PREMIO_PROGRAMMATO_CONCLUSO (AnnoCampionato, Data) AS (
+    SELECT AnnoCampionato, Data
+    FROM GRAN_PREMIO_PROGRAMMATO
+    WHERE Concluso = TRUE
+    ORDER BY AnnoCampionato DESC, Data DESC
+    LIMIT 1)
 SELECT P.*, PP.Prezzo
-FROM SCUDERIA_PARTECIPANTE SP JOIN INGAGGIO_PILOTA IP JOIN PILOTA P JOIN PREZZO_PILOTA PP
+FROM SCUDERIA_PARTECIPANTE SP JOIN INGAGGIO_PILOTA IP JOIN PILOTA P JOIN PREZZO_PILOTA PP JOIN ULTMO_GRAN_PREMIO_PROGRAMMATO_CONCLUSO U
     ON SP.AnnoCampionato = IP.AnnoCampionato AND SP.NomeScuderia = IP.NomeScuderia
     AND P.Codice = IP.CodicePilota
     AND PP.CodicePilota = P.Codice AND PP.AnnoCampionato = SP.AnnoCampionato
-WHERE SP.AnnoCampionato = (SELECT Anno
-                                                FROM CAMPIONATO
-                                                ORDER BY Anno DESC
-                                                LIMIT 1)
-AND PP.DataGranPremio = ( SELECT Data
-                                            FROM GRAN_PREMIO_PROGRAMMATO
-                                            ORDER BY Data DESC
-                                            LIMIT 1)
+WHERE SP.AnnoCampionato = U.AnnoCampionato
+AND PP.DataGranPremio = U.Data
 ORDER BY PP.Prezzo DESC ;
 
