2f03f559e7da9d37ca037e434c3b910b7e5507a7
Alessandro Venturini <ales.ventus21@gmail.com>
2022-07-19 17:06:01 +0200

    Made O10 a procedure. Added relative Trigger

--- Query/O10 - Reset del Punteggio dell’Utente.sql
+++ Query/O10 - Reset del Punteggio dell’Utente.sql
@@ -1,6 +1,26 @@
--- O10 - Reset del Punteggio dell’Utente
+-- O10 - Reset del Punteggio degli Utenti
 
-UPDATE UTENTE
-SET PunteggioCorrente = 0
-WHERE Username = 'CiccioCarluz';
--- anche README
+CREATE PROCEDURE resetPunteggioUtenti()
+BEGIN
+
+    UPDATE UTENTE
+    SET PunteggioCorrente = 0
+    WHERE TRUE;
+
+END;
+
+CREATE TRIGGER nuovoCampionato_INSERT AFTER INSERT ON CAMPIONATO
+FOR EACH ROW
+BEGIN
+
+    IF EXISTS(  SELECT *
+                    FROM GRAN_PREMIO_PROGRAMMATO
+                    WHERE AnnoCampionato != NEW.Anno
+                    AND Concluso = FALSE )
+    THEN
+        SIGNAL SQLSTATE '45000';
+    ELSE
+        CALL resetPunteggioUtenti();
+    END IF;
+
+END;
