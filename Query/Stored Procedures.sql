5cc7e0eb7c7bb306b8460c33734bd93e956a01dd
Alessandro Venturini <ales.ventus21@gmail.com>
2022-07-20 10:54:54 +0200

    Updated O3 more efficient.
    closes #1

--- Query/Stored Procedures.sql
+++ Query/Stored Procedures.sql
@@ -72,12 +72,11 @@ CREATE PROCEDURE visualizzazionePilotiSquadra (IN user VARCHAR(255), IN annoC IN
 BEGIN
 
     SELECT P.*, PGP.Prezzo
-    FROM UTENTE U JOIN SQUADRA S JOIN SCELTA_PILOTA SP JOIN PILOTA_IN_GRAN_PREMIO PGP JOIN PILOTA P
-        ON U.Username = S.UsernameUtente
-        AND SP.UsernameUtente = U.Username AND SP.AnnoCampionato = S.AnnoCampionato AND SP.DataGranPremio = S.DataGranPremio
+    FROM SQUADRA S JOIN SCELTA_PILOTA SP JOIN PILOTA_IN_GRAN_PREMIO PGP JOIN PILOTA P
+        ON SP.UsernameUtente = S.UsernameUtente AND SP.AnnoCampionato = S.AnnoCampionato AND SP.DataGranPremio = S.DataGranPremio
         AND PGP.CodicePilota = SP.CodicePilota AND PGP.AnnoCampionato = SP.AnnoCampionato AND PGP.DataGranPremio = S.DataGranPremio
         AND P.Codice = PGP.CodicePilota
-    WHERE U.Username = user
+    WHERE S.UsernameUtente = user
     AND S.AnnoCampionato = annoC
     AND S.DataGranPremio = dataGP;
 
